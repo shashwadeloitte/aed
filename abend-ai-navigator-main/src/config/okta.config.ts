e3b0d1b14902db5bf256e8d3eab4bdea0c4f4503
Gupta <shashwagupta@deloitte.com>
2025-07-15 13:24:35 +0530

    dist zip

--- abend-ai-navigator-main/src/config/okta.config.ts
+++ abend-ai-navigator-main/src/config/okta.config.ts
@@ -3,7 +3,7 @@ import { OktaAuth, OktaAuthOptions } from '@okta/okta-auth-js';
 export const oktaConfig: OktaAuthOptions = {
   clientId: "0oasg60pytc4rGJZ0697",
   issuer: "https://trial-8955355.okta.com/oauth2/default",
-  redirectUri: "http://localhost:3000/login/callback",
+  redirectUri: `${window.location.origin}/login/callback`,
   scopes: ["openid", "profile", "email"],
   pkce: true,
   responseType: ["code"],
